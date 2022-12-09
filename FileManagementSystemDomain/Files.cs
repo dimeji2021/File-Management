9354171066108086cdca68bc8ebb356437f7f21c
Dimeji <adedimeji88@yahoo.com>
2022-12-09 15:40:52 +0100

    added more logic to the file service folder

---
+++ FileManagementSystemDomain/Files.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManagementSystemDomain
+{
+    public class Files
+    {
+        public string Name { get; set; }
+        public string Extension { get; set; }
+        public string Path { get; set; }
+        public long FileSizeBytes { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public bool IsTextFile { get; set; }
+        public string TextContents { get; set; }
+    }
+}
--- FileManagementSystemService/FileService.cs
+++ FileManagementSystemService/FileService.cs
@@ -1,33 +1,108 @@
-﻿using Microsoft.AspNetCore.Http;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FileManagementSystemDomain;
+using Microsoft.AspNetCore.Http;
+using SautinSoft.Document;
+//using ServiceStack;
 
 namespace FileManagementSystemService
 {
     public class FileService : IFileService
     {
         private static string rootPath = Directory.GetCurrentDirectory();
-        private IHttpContextAccessor _httpContextAccessor;
-        public string UploadFile(IFormFile file, string? FolderPath)
+        //public string UploadFile(IFormFile file, string? FolderPath)
+        //{
+        //    try
+        //    {
+        //        string siteHost = (_httpContextAccessor.HttpContext.Request.IsHttps ? "https" : "http" + "://" + _httpContextAccessor.HttpContext.Request.Host);
+        //        if (FolderPath == null)
+        //        {
+        //            file.CopyTo(new FileStream(rootPath, FileMode.Create, FileAccess.Write));
+        //            return "File created at: " + Path.Combine(rootPath, file.Name);
+        //        }
+        //        file.CopyTo(new FileStream(Path.Combine(rootPath, FolderPath), FileMode.Create, FileAccess.Write));
+        //        return "File created at: " + Path.Combine(rootPath, FolderPath, file.Name);
+        //    }
+        //    catch (Exception)
+        //    {
+        //        throw;
+        //    }
+        //}
+        //public string CreateFile(string path)
+        //{
+        //    if (!File.Exists(path))
+        //    {
+        //        using (FileStream fs = File.Create(path))
+        //        {
+        //            return fs.Name;
+        //        }; 
+        //    }
+        //   return "File \"{0}\" already exists.";
+        //}
+        //public void DeleteFiles(string file)
+        //{
+        //    if (File.Exists(file))
+        //    {
+
+        //        try
+        //        {
+        //           File.Delete(file);
+        //        }
+        //        catch (IOException e)
+        //        {
+        //            throw e; 
+        //        }
+        //    }
+        //}
+
+        public string CreateFilesByFolderName(string FolderName, string FileName)
         {
-            try
+            var filepath = Path.Combine(rootPath, FolderName, FileName);
+            File.Create(filepath);
+            return filepath;
+        }
+        public string DeleteFiles(string filepath)
+        {
+            if (File.Exists(filepath))
             {
-                string siteHost = (_httpContextAccessor.HttpContext.Request.IsHttps ? "https" : "http" + "://" + _httpContextAccessor.HttpContext.Request.Host);
-                if (FolderPath == null)
-                {
-                    file.CopyTo(new FileStream(rootPath, FileMode.Create, FileAccess.Write));
-                    return "File created at: " + Path.Combine(rootPath, file.Name);
-                }
-                file.CopyTo(new FileStream(Path.Combine(rootPath, FolderPath), FileMode.Create, FileAccess.Write));
-                return "File created at: " + Path.Combine(rootPath, FolderPath, file.Name);
+                File.Delete(filepath);
+                return "sucessfully deleted";
             }
-            catch (Exception)
+            return "File does not exist";
+        }
+        public string[] GetAllFilesByFolderName(string folderName)
+        {
+            var files = Directory.GetFiles(rootPath, "*", SearchOption.TopDirectoryOnly);
+            return files;
+        }
+        public string[] GetAllFileContentByFilePath(string filepath)
+        {
+            if (Directory.Exists(filepath))
             {
-                throw;
+                return Directory.GetFiles(filepath);
             }
+            return new string[0];
+        }
+        public string UpdateFileContent(string filepath, string newContent)
+        {
+            if (File.Exists(filepath))
+            {
+                File.WriteAllText(filepath, newContent);
+                return "Successful";
+            }
+            return "Update Unsuccessful";
+        }
+        public string ReadFileContent(string filepath, string newContent)
+        {
+            if (File.Exists(filepath))
+            {
+                return File.ReadAllText(filepath);
+            }
+            return "file does not exist";
+        }
+        public string RenameFile(string filepath, string newfileName)
+        {
+            var newFilePath = Path.Combine(Path.GetDirectoryName(filepath), newfileName + Path.GetExtension(filepath));
+            File.Move(filepath, newFilePath);
+            return "File rename successfully";
         }
     }
 }
